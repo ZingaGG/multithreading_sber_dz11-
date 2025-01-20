0984536d743aedd4ea30219ee9ac4eed4a959ebf
ZingaGG <trad_bar@mail.ru>
2025-01-20 15:21:03 +0300

    Small changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,22 +7,22 @@ import org.example.ThreadPools.ThreadPool;
 public class Main {
     public static void main(String[] args) {
         // Пример с фиксированным пулом
-//        {
-//            ThreadPool fixedThreadPool = new FixedThreadPool(3);
-//            fixedThreadPool.start();
-//            for (int i = 0; i < 5; i++) {
-//                int finalI = i;
-//                fixedThreadPool.execute(() -> {
-//                    System.out.println("FixedThreadPool: Task " + finalI + " has been completed in " + Thread.currentThread().getName());
-//                });
-//            }
-//        }
+        {
+            ThreadPool fixedThreadPool = new FixedThreadPool(3);
+            fixedThreadPool.start();
+            for (int i = 0; i < 5; i++) {
+                int finalI = i;
+                fixedThreadPool.execute(() -> {
+                    System.out.println("FixedThreadPool: Task " + finalI + " has been completed in " + Thread.currentThread().getName());
+                });
+            }
+        }
 
         // Пример с расширяемым пулом
         {
             ThreadPool scalableThreadPool = new ScalableThreadPool(2, 5);
             scalableThreadPool.start();
-            for (int i = 0; i < 6; i++) { // можно менять i для наглядности, чтобы увидеть сужение и расширения пула
+            for (int i = 0; i < 15; i++) { // можно менять i для наглядности, чтобы увидеть сужение и расширения пула
                 int finalI = i;
                 scalableThreadPool.execute(() -> {
                     System.out.println("ScalableThreadPool: Task " + finalI + " has been completed in " + Thread.currentThread().getName());
